42a9f56193866fe061d56e18c49bfc93d857ce36
Rachael Shaw <rachael.shaw@keethealth.com>
2022-03-13 17:46:31 -0500

    add requestAnimationFrame and dot creation

--- src/index.js
+++ src/index.js
@@ -1,15 +1,34 @@
 let request;
+let i = 0;
 
-const button = document.getElementById("btn")
+const button = document.getElementById('btn');
+const game = document.getElementById('game');
 let turnedOn = false
 
-button.addEventListener('click', e => {
 
+button.addEventListener('click', e => {
   if (turnedOn) {
-    turnedOn = false
-    e.currentTarget.textContent = 'Stop'
+    turnedOn = false;
+    e.currentTarget.textContent = 'Stop';
+    startAnimation();
   } else {
     turnedOn = true;
-    e.currentTarget.textContent = 'Start'
+    e.currentTarget.textContent = 'Start';
+    stopAnimation();
   }
 })
+
+
+function startAnimation() {
+    game.innerHTML += '<div class="absolute w-10 h-10 rounded-full bg-fun-green"></div>'
+    requestAnimationFrame(performAnimation);
+  }
+  function stopAnimation() {
+    cancelAnimationFrame(request);
+  }
+
+  function performAnimation() {
+    request = requestAnimationFrame(performAnimation);
+    console.log('request', request);
+    console.log(i++);
+  }
