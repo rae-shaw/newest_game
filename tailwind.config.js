133f94b1838cf0f356d8ff024467f825f4713ed3
Rachael Shaw <rachael.shaw@keethealth.com>
2022-03-13 16:37:01 -0500

    updated styling, added colors to config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   content: [
     "./src/**/*.{html,js}",
     "index.html"
   ],
   theme: {
+    colors: {
+      'fun-green': '#00bfb2',
+      'background-grey': '#d0d3d4',
+      ...colors,
+    },
     extend: {},
   },
   plugins: [],
