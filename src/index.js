0f556e052219e60a6ccf76d4272b59ed87dc6a58
Rachael Shaw <rachael.shaw@keethealth.com>
2022-03-14 19:57:52 -0500

    add slider functionality

--- src/index.js
+++ src/index.js
@@ -8,27 +8,49 @@ let turnedOn = false
 
 button.addEventListener('click', e => {
   if (turnedOn) {
-    turnedOn = false;
-    e.currentTarget.textContent = 'Stop';
-    startAnimation();
+    turnedOn = false
+    e.currentTarget.textContent = 'Pause'
+    startAnimation()
   } else {
     turnedOn = true;
-    e.currentTarget.textContent = 'Start';
-    stopAnimation();
+    e.currentTarget.textContent = 'Start'
+    stopAnimation()
   }
 })
 
+const slider = document.getElementById("game-slider");
+const output = document.getElementById("speed");
+output.innerHTML = slider.value; // Display the default slider value
+
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
+
+// const diameter = 10 + Math.floor(Math.random() * 41);
+
+const diameter = 50
+const xPosition = 10
+const yPosition = 10
 
 function startAnimation() {
+  console.log(diameter)
     game.innerHTML += '<div class="absolute w-10 h-10 rounded-full bg-fun-green"></div>'
-    requestAnimationFrame(performAnimation);
+    requestAnimationFrame(performAnimation)
   }
-  function stopAnimation() {
-    cancelAnimationFrame(request);
+function stopAnimation() {
+    game.innerHTML = ''
+    cancelAnimationFrame(request)
   }
 
   function performAnimation() {
-    request = requestAnimationFrame(performAnimation);
-    console.log('request', request);
-    console.log(i++);
+    request = requestAnimationFrame(performAnimation)
   }
+
+  // logs for slider
+  // points increase when clicking dot
+  // points only increase when in moving state
+  // dot "pops" when clicked on
+  // inline styles/using variable for dot in innerHTML
+  // dots appear @top
+  // dots appear with random sizes
+  // dots move down from top to bottom (tailwind animation config?)
