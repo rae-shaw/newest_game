a66226b949b6b6fadeee582bc5505a398d14f497
Rachael Shaw <rachael.shaw@keethealth.com>
2022-03-20 09:59:38 -0500

    update getElement

--- src/index.js
+++ src/index.js
@@ -6,14 +6,26 @@ const colors = ['#00bfb2', '#d3273e', '#e56db1', '#41b6e6']
 
 const button = document.getElementById('btn')
 const gameArea = document.getElementById('game')
+const height = gameArea.offsetHeight
+const width = gameArea.offsetWidth
+var testElements = document.getElementsByClassName('dot');
+var testDivs = Array.prototype.filter.call(testElements, function(testElement){
+  return testElement.nodeName === 'DIV';
+});
+
 const slider = document.getElementById('game-slider')
 const output = document.getElementById('speed')
 const scoreTrack = document.getElementById("score")
 let turnedOn = false
 let dotValue;
 const dots = () => {
-  document.getElementsByClassName('.dot')
+  document.getElementsByClassName('dot')
 }
+
+function getSpeed() {
+  return parseInt(output.value, 10);
+}
+
 
 button.addEventListener('click', e => {
   if (!turnedOn) {
@@ -28,7 +40,8 @@ button.addEventListener('click', e => {
 })
 
 function game() {
-  createDot();
+  createDot()
+  animateDots()
 }
 
 // slider
@@ -45,24 +58,46 @@ function clickedDot() {
     let dot = this
     this.parentNode.removeChild(dot);
   } else {
+    confirm('Press start to continue popping!')
   }
 }
 
 function createDot() {
-  const span = document.createElement('div')
+  const span = document.createElement('div', {id: "myId", name: "myName"})
   const dotSize = getRandomInt(10, 100).toString()
   const dotColor = getRandomColor(colors)
+  const leftPosition = getRandomInt(0, width)
+  const topPosition = 0 - dotSize - getSpeed()
   dotValue = calcValue(dotSize)
-  span.setAttribute = ('class', 'dot')
+
+  span.setAttribute('class', 'dot')
   span.style.width = `${dotSize}px`
   span.style.height = `${dotSize}px`
   span.style.backgroundColor = dotColor
   span.style.borderRadius = '50%'
-  span.style.top = '10px';
-  span.style.left = '10px';
+  span.style.top = `${topPosition}px`
+  span.style.left = `${leftPosition}px`
   span.addEventListener('click', clickedDot)
   gameArea.append(span);
+  console.log(span);
 }
+
+function animateDots() {
+  const dots = document.querySelectorAll('.dot')git add 
+  let speed = getSpeed()
+  for (let i = 0; i < dots.length; i++) {
+    let positionY = parseInt(dots[i].style.top, 10)
+    let velocity = positionY += speed
+    
+    if (positionY > height) {
+      let dot = this
+      this.parentNode.removeChild(dot);
+    }
+
+    dots[i].style.top = velocity + "px";
+  }
+}
+
 function getRandomColor(arr) {
    return arr[Math.floor(Math.random() * arr.length)]
  }
