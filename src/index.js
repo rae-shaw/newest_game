019b4d102bcd5eb9fbb9ba3ffb0f2cbd0302d77a
Rachael Shaw <rachael.shaw@keethealth.com>
2022-05-01 10:05:50 -0500

    using Raf in array

--- src/index.js
+++ src/index.js
@@ -28,29 +28,34 @@ const height = parseInt(computedStyles.getPropertyValue('height').replace('px', 
 const bottomOfGameScreen = height * 1.5;
 const width = parseInt(computedStyles.getPropertyValue('width').replace('px', ''))
 
-
 // on click action
-// button.addEventListener('click', e => {
-//   // when the button is clicked to start the game, the following happens:
-//   if (!turnedOn) {
-//     turnedOn = true
-//     e.currentTarget.textContent = 'PAUSE'
-//     // request = requestAnimationFrame(move)
-//     popUp.classList.add("hidden")
-//   } else {
-//     // when the button is clicked to pause the game, the following happens:
-//     turnedOn = false
-//     e.currentTarget.textContent = 'START'
-//     // request = cancelAnimationFrame(request)
-//     popUp.classList.remove("hidden")
-//   }
-// })
+button.addEventListener('click', e => {
+  // when the button is clicked to start the game, the following happens:
+  console.log(turnedOn)
+  if (!turnedOn) {
+    turnedOn = true
+    e.currentTarget.textContent = 'PAUSE'
+    popUp.classList.add("hidden")
+    animate([
+      {
+        time: 2,
+        run: function() {
+          let currentPosition = parseInt(dot.style.top.slice(0, -2))
+            if (currentPosition > bottomOfGameScreen) {
+              dot.remove()
+            }
+            dot.style.top = `${pixelIncrement + currentPosition}px`
+        }
+      }
+    ]);
+  } else {
+    // when the button is clicked to pause the game, the following happens:
+    turnedOn = false
+    e.currentTarget.textContent = 'START'
+    popUp.classList.remove("hidden")
+  }
+})
 
-// recursive function using requestAnimationFrame to slide the dots down the screen
-// function move() {
-//   animateDots()
-//   request = requestAnimationFrame(move)
-// }
 
 //_______________________________________________________//
 //                  slider                               //
@@ -64,72 +69,25 @@ const width = parseInt(computedStyles.getPropertyValue('width').replace('px', ''
 //                   dot                                 //
 //_______________________________________________________//
 // create a dot
-// function createDot() {
-//   console.log('createDot')
-//   const span = document.createElement('div')
-//   const dotSize = getRandomInt(10, 100)
-//   const leftPosition = getRandomInt(0, width)
-//   const dotValue = calcValue(dotSize)
+function createDot() {
+  console.log('createDot')
+  const span = document.createElement('div')
+  const dotSize = getRandomInt(10, 100)
+  const leftPosition = getRandomInt(0, width)
+  // const dotValue = calcValue(dotSize)
 
-//   span.setAttribute('class', 'dot')
-//   span.style.borderRadius = '50%'
-//   span.style.position = 'absolute'
-//   // span.style.width = `${dotSize}px`
-//   // span.style.height = `${dotSize}px`
-//   // span.style.backgroundColor = '#FB6970'
-//   span.style.top = '-100px'
-//   span.style.cursor = 'pointer'
-//   span.style.left = `${leftPosition}px`
-//   // span.style.overflow = 'inherited'
-//   // span.addEventListener('click', function () { clickedDot(this, dotValue) }, 'false')
-//   gameArea.append(span)
-// }
-
-// action for when a dot is clicked
-// function clickedDot(el, val) {
-//   if (turnedOn) {
-//     score += val
-//     scoreTrack.innerHTML = score
-//     el.remove()
-//   }
-// }
-
-// move dots down the screen
-// function animateDots() {
-//   const pixelIncrement = slider.value/10
-//   let dots = document.querySelectorAll(".dot")
-//   dots.forEach((dot) => {
-//     let currentPosition = parseInt(dot.style.top.slice(0, -2))
-//     if (currentPosition > bottomOfGameScreen) {
-//       dot.remove()
-//     }
-//     dot.style.top = `${pixelIncrement + currentPosition}px`
-//   });
-// }
-
-// calculate the value of dot based on the size
-// function calcValue(size) {
-//   if (size <= 20) {
-//     return 10
-//   }
-//   if (size <= 40) {
-//     return 7
-//   }
-//   if (size <= 60) {
-//     return 5
-//   }
-//   if (size <= 80) {
-//     return 3
-//   }
-//   return 1
-// }
-
-// helper function to get a random size for the 
-// function getRandomInt(min, max) {
-//   min = Math.ceil(min)
-//   max = Math.floor(max)
-//   return Math.floor(Math.random() * (max - min + 1) + min); 
-// }
+  span.setAttribute('class', 'dot')
+  span.style.borderRadius = '50%'
+  span.style.position = 'absolute'
+  span.style.width = `${dotSize}px`
+  span.style.height = `${dotSize}px`
+  span.style.backgroundColor = '#FB6970'
+  span.style.top = '-100px'
+  span.style.cursor = 'pointer'
+  span.style.left = `${leftPosition}px`
+  span.style.overflow = 'inherited'
+  gameArea.append(span)
+}
 
 
 // --- Global
@@ -141,34 +99,44 @@ const MAX_POSITION = 575
 // ---- Request animation frame with delta fix
 
 const dots = document.querySelector('.dot')
-let positionRafDelta = []
-// let lastRafUpdate = getTime();
-let lastRafUpdate = []
 
-function animateRafDelta() {
-  const now = getTime()
-  const delta = (now - lastRafUpdate) / FRAME_DURATION;
-  lastRafUpdate[i] = getTime()
-  for (let i = 0; i < dots.length; i++) {
-    //getTime()
-    //update to now
-    positionRafDelta[i] += slider.value * .1 * delta;
 
-    // Reset position
-    // if (positionRafDelta > MAX_POSITION) {
-    //   dot.remove()
-    // }
+function animate(dots) {
+  let item
+  let duration
+  let end = 0;
+  console.log('in animate', item);
 
-  // Update position
-    dots[i].style.transform = `translateY(${ positionRafDelta[i] }px)`;
+  const step = function() {
 
-    // Update last updated time
-    lastRafUpdate[i] = now;
+    let current = +new Date()
+    let = remaining = end - current;
 
+    if(remaining < 60) {
+
+      if (item) {
+        item.run(1)  //1 = progress is at 100%
+
+        item = dots.shift()  //get the next item
+      }
+
+      if(item) {
+        duration = item.time*1000;
+        end = current + duration;
+        item.run(0);  //0 = progress is at 0%
+      } else {
+        return;
+      }
+
+    } else {
+      let rate = remaining/duration;
+      rate = 1 - Math.pow(rate, 3);  //easing formula
+      item.run(rate);
+    }
+
+    requestAnimationFrame(step);
   }
-
-
-  request = requestAnimationFrame(animateRafDelta);
+  step();
 }
 
 button.addEventListener('click', e => {
@@ -176,13 +144,11 @@ button.addEventListener('click', e => {
   if (!turnedOn) {
     turnedOn = true
     e.currentTarget.textContent = 'PAUSE'
-    request = requestAnimationFrame(animateRafDelta)
     popUp.classList.add("hidden")
   } else {
     // when the button is clicked to pause the game, the following happens:
     turnedOn = false
     e.currentTarget.textContent = 'START'
-    request = cancelAnimationFrame(request)
     popUp.classList.remove("hidden")
   }
 })
